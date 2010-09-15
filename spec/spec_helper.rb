e370b5db4a2ee30c6e2484d4ab5170c0ae14f8e7
Nelson <nelson.dias.andre@gmail.com>
2010-09-15 17:51:20 +0100

    sign in , sign out with errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,5 +59,9 @@ RSpec.configure do |config|
     # railties/lib/rails/application/bootstrap.rb
     ActiveSupport::Dependencies.clear
 
+def test_sign_in(user)
+    controller.current_user = user
+  end
+
 
 end
